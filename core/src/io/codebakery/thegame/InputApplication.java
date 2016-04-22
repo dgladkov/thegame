4b767a67fb006cddb1ec210ac27947de7c8b8cae
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2016-04-22 19:40:10 +0300

    initial commit

---
+++ core/src/io/codebakery/thegame/InputApplication.java
@@ -0,0 +1,31 @@
+package io.codebakery.thegame;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.InputAdapter;
+
+
+public abstract class InputApplication extends InputAdapter implements ApplicationListener {
+    @Override
+    public void create() {
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void render() {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+    }
+}
