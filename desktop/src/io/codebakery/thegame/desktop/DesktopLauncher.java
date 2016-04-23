309668d12d597671fd304503d3ba955c594b5c66
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2016-04-23 11:37:06 +0300

    set desktop window properties

--- desktop/src/io/codebakery/thegame/desktop/DesktopLauncher.java
+++ desktop/src/io/codebakery/thegame/desktop/DesktopLauncher.java
@@ -5,8 +5,12 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import io.codebakery.thegame.TheGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new TheGame(), config);
-	}
+    public static void main (String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "The Game";
+        config.width = 1280;
+        config.height = 720;
+        config.resizable = false;
+        new LwjglApplication(new TheGame(), config);
+    }
 }
